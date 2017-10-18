cc3c87c25463d4f8844cd4750da6eb1d3678694e
Whymess <Jonathan.lev.messing@gmail.com>
2017-10-18 11:59:43 -0400

    the famous last words, last css commit

--- src/Components/TopGreeting/TopGreeting.js
+++ src/Components/TopGreeting/TopGreeting.js
@@ -3,8 +3,10 @@ import '../../CSS/Greet.css';
 
 export default (props) => { 
 	return (
-	  	
-	  		<div className="Greet_person"> Hi, Mike </div>
-	 
+	  		<div className="top_greeting">
+	  			<div className="teamName"> TeamPage </div>
+		  		<img className="main_teamLogo" src={require('../../Assets/Images/logo.png')} alt="Mckinsey And Company"/>
+		  		<div className="Greet_person"> Hi, Mike </div>
+	 		</div>
 	 )
 }
